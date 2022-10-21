49960f8adb74a9b87c81eb29eb34241879ae55cb
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-10-21 16:29:37 -0300

    feat: add unit test car controller create

--- src/tests/unit/controllers/seu-test-controller.test.ts
+++ src/tests/unit/controllers/seu-test-controller.test.ts
@@ -1,22 +1,43 @@
-// template para criação dos testes de cobertura da camada de controller
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import { Request, Response } from 'express';
+import CarModel from '../../../models/CarModel';
+import CarService from '../../../services/CarService';
+import CarController from '../../../controllers/CarController';
+import { carMock, carMockWithId } from '../../mocks/carMock';
 
+describe('Sua descrição', () => {
+  const carModel = new CarModel()
+  const carService = new CarService(carModel);
+  const carController = new CarController(carService);
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+  const req = {} as Request; 
+  const res = {} as Response;
 
-// describe('Sua descrição', () => {
+  before(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  afterEach(() => {
+    sinon.restore()
+  })
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  describe('Create Frame', () => {
+    beforeEach(() => {
+      sinon.stub(carService, 'create').resolves(carMock);
+    })
 
-//   it('', async () => {});
+    it('Success', async () => {
+      req.body = carMock;
+      await carController.create(req, res);
 
-// });
+      const statusStub = res.status as sinon.SinonStub;
+      expect(statusStub.calledWith(201)).to.be.true;
+
+      const jsonStub = res.json as sinon.SinonStub;
+      expect(jsonStub.calledWith(carMockWithId)).to.be.true;
+    });
+  });
+});
